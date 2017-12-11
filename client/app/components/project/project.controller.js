0abdbb883fda0ed7adbe36f5de3b5c18d8e2ee90
Oleg <belinskiy21@gmail.com>
2017-12-12 00:15:45 +0200

    delete project and edit project done

--- client/app/components/project/project.controller.js
+++ client/app/components/project/project.controller.js
@@ -2,19 +2,41 @@ class ProjectController {
   constructor() {
     let model = this;
 
-    model.projects = [{'title': 'My first project'},
-    {'title': 'My second project'},
-    {'title': 'My another project'}];
+    model.projects = [{'id': 1,'title': 'My first project'},
+    {'id': 2, 'title': 'My second project'},
+    {'id': 3, 'title': 'My another project'}]
+
 
 
     model.addProject = () => {
-      if(!model.title || model.title === '') { return; }
-      model.projects.push({title: model.title});
-      model.title = '';
-    };
+      if(!model.title || model.title === '') { return }
+      model.projects.push({title: model.title})
+      model.title = ''
+    }
     model.removeContent = () => {
-      model.title = '';
-    };
+      model.title = ''
+    }
+    model.deleteProject = (project) => {
+      if (confirm("sure to delete?"))
+      model.projects.splice(model.projects.indexOf(project), 1 )
+    }
+    model.editProject = (project) => {
+      model.showEdit = true
+      model.editTitle = project.title
+      model.helpEdit = project
+    }
+
+
+    model.Save = function ()
+    {
+      model.newProject = { "id": model.helpEdit.id, "title": model.editTitle }
+      model.editTitle = model.projects.splice( model.projects.indexOf(model.helpEdit), 1 )
+      model.projects.push(model.newProject)
+      model.editTitle=""
+      model.showEdit = false
+      console.log(model.editTitle)
+    }
+
   }
 }
 
